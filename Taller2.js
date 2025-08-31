f41eabc25cc27057fde86f9c1dae468271f7b045
Alejandro <da.hurtado09@ciaf.edu.co>
2025-08-31 18:16:57 -0500

    Primer commit

---
+++ Taller2.js
@@ -0,0 +1,11 @@
+const students = [
+  { id: 1, name: "Ana", age: 20, grade: 85, course: "JavaScript" },
+  { id: 2, name: "Carlos", age: 22, grade: 92, course: "Python" },
+  { id: 3, name: "María", age: 19, grade: 78, course: "JavaScript" },
+  { id: 4, name: "Luis", age: 21, grade: 88, course: "Java" },
+  { id: 5, name: "Sofía", age: 20, grade: 95, course: "JavaScript" }
+];
+// Encuentra todos los estudiantes de JavaScript con nota mayor a 80
+
+const mayorque80 = students.filter(alumnos => alumnos.course === "JavaScript" && alumnos.grade > 80);
+console.log(mayorque80)
